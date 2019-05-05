cdfb0dd093aedfc41c931e1d7f11770dfeaa5ad2
yokoto <youkotoushi@gmail.com>
2019-05-05 17:49:43 +0900

    #5 Add User model and Modify i18n error messages in Devise

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :name, presence: true, length: { maximum: 50 }
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
