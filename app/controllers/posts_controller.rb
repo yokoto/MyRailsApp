feb33809a25205b00fff8ddfb70575d77c96decc
yokoto <youkotoushi@gmail.com>
2019-05-07 17:24:54 +0900

    Add some to display, paginate, and delete Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,30 @@
 class PostsController < ApplicationController
+  before_action :correct_user, only: :destroy
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:success] = "投稿されました。"
+      redirect_to root_url
+    else
+      render root_path
+    end
+  end
+
+  def destroy
+    @post.destroy
+    flash[:success] = "投稿が削除されました。"
+    redirect_to request.referrer || root_url # 1つ前のURLか、なければrootを返す
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:content)
+    end
+
+    def correct_user
+      @post = current_user.posts.find_by(id: params[:id])  # 現在のユーザーが削除対象のマイクロポストを保有してるかどうか
+      redirect_to root_url if @post.nil?
+    end
 end
