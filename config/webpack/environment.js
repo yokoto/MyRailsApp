218593c4c4c4536b14955d190446dd77d1c8d0d3
Toshihiro Yokora <yokoto@Toshihiro-no-MacBook-Pro.local>
2019-07-05 11:16:55 +0900

    Setup TypeScript

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,10 @@
 const { environment } = require('@rails/webpacker')
+const typescript =  require('./loaders/typescript')
 
-module.exports = environment
+environment.loaders.prepend('typescript', typescript)
+module.exports = {
+  test: /\.(ts|tsx)?(\.erb)?$/,
+  use: [{
+    loader: 'awesome-typescript-loader'
+}]
+}
